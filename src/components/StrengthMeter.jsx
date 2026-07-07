96eba4117fa6496580f6d4dd771ae7b16f77956b
Joe Garcia <joegarcia12_al@hotmail.com>
2026-07-07 15:12:32 -0600

    Se añade una barra visual en el archivo StrengthMeter.jsx, en la funcion del mismo nombre

--- src/components/StrengthMeter.jsx
+++ src/components/StrengthMeter.jsx
@@ -13,13 +13,22 @@ const StrengthMeter = ({password}) => {
     }
 
     const currentColor = colors[label];
-
+    //Formula para convertir cualquier valor en una escala
+    let porcentajeBarra = (score / 60) * 100
+    
     return (
         <div>
-        <p style={{ color:currentColor }}>{label}</p>
+        <p style={{ color:currentColor }} >{label}</p>
+        <div style={{width: '40%', backgroundColor:'#e0e0e0', height:'10px', margin: '0 auto'}}>
+         
+         <div style={{width:`${porcentajeBarra}%`, backgroundColor:currentColor, height:'100%'}}></div>
+        
+        </div>
         </div>
     );
 }
+
+
 
 
 export default StrengthMeter;
