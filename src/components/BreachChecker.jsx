2917e41574433905d63db2b65236bc1f3d5ba630
Joe Garcia <joegarcia12_al@hotmail.com>
2026-07-11 22:59:13 -0600

    Añadimos el componente BreachChekcer con la integracion de HIBP API

---
+++ src/components/BreachChecker.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { hashPassword } from "../utils/hashPassword";
+import { checkPasswordBreach } from "../utils/checkPasswordBreach";
+
+const BreachChecker = ({password}) => {
+    const [resultado, setResultado] = useState(null);
+    const [cargando, setCargando] = useState(false);
+
+const handleVerificar = async () => {
+    setCargando(true);
+    
+    const hash = await hashPassword(password);
+    const resultadoBreach = await checkPasswordBreach(hash);
+
+    setResultado(resultadoBreach);
+    setCargando(false);
+    }
+return (
+        <div>
+            <button onClick={handleVerificar}>Verificar contraseña</button>
+            {cargando && <p>Verificando...</p>}
+            {resultado && resultado.breached && <p>Esta contraseña ha sido filtrado {resultado.count}veces</p>}
+            {resultado && !resultado.breached && <p>No aparecen filtraciones conocidas</p>}
+        </div>)
+}
+
+export default BreachChecker;
